d624c382d04cb3725bf8ea48b021cd8ce536b570
Jonathan Donaldson <jon@than.dev>
2022-02-18 01:43:00 +0000

    Add logging of packets

--- ebpfapp/src/main.rs
+++ ebpfapp/src/main.rs
@@ -1,12 +1,16 @@
 use anyhow::Context;
-use aya::maps::perf::PerfEventArray;
+use aya::maps::perf::AsyncPerfEventArray;
 use aya::programs::{Xdp, XdpFlags};
+use aya::util::online_cpus;
 use aya::{include_bytes_aligned, Bpf};
+use bytes::BytesMut;
+use ebpfapp_common::PacketLog;
 use log::info;
 use simplelog::{ColorChoice, ConfigBuilder, LevelFilter, TermLogger, TerminalMode};
 use std::convert::{TryFrom, TryInto};
+use std::net;
 use structopt::StructOpt;
-use tokio::signal;
+use tokio::{signal, task};
 
 #[derive(Debug, StructOpt)]
 struct Opt {
@@ -40,9 +44,30 @@ async fn main() -> Result<(), anyhow::Error> {
     program.attach(&opt.iface, XdpFlags::SKB_MODE)
         .context("failed to attach the XDP program with default flags - try changing XdpFlags::default() to XdpFlags::SKB_MODE")?;
 
+    let mut perf_array = AsyncPerfEventArray::try_from(bpf.map_mut("EVENTS")?)?;
 
-    let mut perf_array = PerfEventArray::try_from(bpf.map_mut("EVENTS")?)?;
+    for cpu_id in online_cpus()? {
+        let mut buf = perf_array.open(cpu_id, None)?;
 
+        task::spawn(async move {
+            let mut buffers = (0..10)
+                .map(|_| BytesMut::with_capacity(1024))
+                .collect::<Vec<_>>();
+
+            loop {
+                let events = buf.read_events(&mut buffers).await.unwrap();
+                for i in 0..events.read {
+                    let buf = &mut buffers[i];
+                    let ptr = buf.as_ptr() as *const PacketLog;
+                    let data = unsafe { ptr.read_unaligned() };
+                    let src_addr = net::Ipv4Addr::from(data.ipv4_address);
+                    println!("LOG: SRC {}, ACTION {}", src_addr, data.action);
+                }
+            }
+        });
+    }
+
+    info!("Listening on {}", &opt.iface);
     info!("Waiting for Ctrl-C...");
     signal::ctrl_c().await?;
     info!("Exiting...");
